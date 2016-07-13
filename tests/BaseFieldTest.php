0179d2e25a599b8fca9e65ea96101d9ebb4c8fe8
Gary Jones <gary@gamajo.com>
2016-07-13 12:24:02 +0100

    Tests: Initial test that tests are working

---
+++ tests/BaseFieldTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: gary
+ * Date: 13/07/2016
+ * Time: 11:56
+ */
+
+namespace ModularWP\Formalize;
+
+use PHPUnit_Framework_TestCase;
+
+class BaseFieldTest extends PHPUnit_Framework_TestCase {
+	public function testTestsAreWorking() {
+		$this->assertTrue( true );
+	}
+}
