9d8cd1d3f830dadac746833d11aad6fdedb45676
Gary Jones <gary@gamajo.com>
2016-07-13 12:25:09 +0100

    Tests: get_default_settings() returns an array

--- tests/BaseFieldTest.php
+++ tests/BaseFieldTest.php
@@ -15,4 +15,22 @@ class BaseFieldTest extends PHPUnit_Framework_TestCase {
 	public function testTestsAreWorking() {
 		$this->assertTrue( true );
 	}
+
+	/**
+	 * @covers ModularWP\Formalize\BaseField::get_default_settings
+	 */
+	public function testGetDefaultSettingsReturnsAnArray(  ) {
+		// Arrange.
+		$base_field = new ConcreteBaseField();
+
+		// Act.
+		$default_settings = $base_field->get_default_settings();
+
+		// Assert.
+		$this->assertInternalType( 'array', $default_settings );
+	}
+}
+
+class ConcreteBaseField extends BaseField {
+	public function generate_field( $instance = [ ] ) {}
 }
